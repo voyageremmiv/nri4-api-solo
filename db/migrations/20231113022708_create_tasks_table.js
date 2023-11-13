9fc809116a941870be4e097fc3a4ef6a829fa6a8
voyageremmiv <voyager.insta@gmail.com>
2023-11-13 11:39:13 +0900

    feature: #3 add a migration and seed file of Tasks

---
+++ db/migrations/20231113022708_create_tasks_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("tasks", function (table) {
+    table.increments("id").primary(); // Set this column as the primary key
+    table.string("title", 64).notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("tasks");
+};
