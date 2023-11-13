a0db52a60b057cc5aa35a06ff5b8042621ac2ca1
voyageremmiv <voyager.insta@gmail.com>
2023-11-13 20:30:23 +0900

    fix: #7 add OPTIONS endpoint for CORS

--- src/server.js
+++ src/server.js
@@ -8,6 +8,14 @@ const setupServer = () => {
     res.status(200).json({ text: "hello world" });
   });
 
+  app.options("/tasks", (req, res) => {
+    res.set({ "Access-Control-Allow-Origin": "*" });
+    res.set({ "Access-Control-Allow-Methods": "GET,POST,PATCH,DELETE" });
+    res.set({ "Access-Control-Allow-Headers": "Content-Type" });
+
+    res.sendStatus(200);
+  });
+
   app.get("/tasks", tasksController.get);
   app.post("/tasks", tasksController.post);
   app.patch("/tasks", tasksController.patch);
